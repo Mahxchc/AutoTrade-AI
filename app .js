55af3dd785df4042f51d15951542d30980777184
Mahxchc <m.davari138600@gmail.com>
2026-08-04 05:51:13 +0330

    Update app .js

--- app .js
+++ app .js
@@ -1,1 +1,65 @@
+let tg = window.Telegram.WebApp;
 
+
+tg.ready();
+tg.expand();
+
+
+
+// اتصال تلگرام
+
+let user = tg.initDataUnsafe.user;
+
+
+if(user){
+
+document.getElementById("user").innerHTML =
+"خوش آمدی " + user.first_name + " 👋";
+
+}
+else{
+
+document.getElementById("user").innerHTML =
+"کاربر مهمان";
+
+}
+
+
+
+
+
+// تغییر صفحات پایین
+
+function openPage(page){
+
+
+let pages = document.querySelectorAll(".page");
+
+
+pages.forEach(p=>{
+
+p.classList.remove("active");
+
+});
+
+
+
+document.getElementById(page).classList.add("active");
+
+}
+
+
+
+
+
+// شروع AI
+
+function startAI(){
+
+
+alert(
+"🤖 ربات معامله‌گر اتوماتیک فعال شد"
+);
+
+
+}
