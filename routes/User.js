ba2592c5f5fef5c9d80263e2d2bf48fdb30cc259
Mahxchc <m.davari138600@gmail.com>
2026-08-06 20:05:17 +0330

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -1,1 +1,117 @@
+// =====================================
+// AutoTrade AI
+// User Routes
+// =====================================
 
+import express from "express";
+import User from "../models/User.js";
+
+
+const router = express.Router();
+
+
+
+// دریافت اطلاعات کاربر
+
+router.get("/:telegramId", async (req,res)=>{
+
+
+    try{
+
+
+        const user = await User.findOne({
+
+            telegramId:req.params.telegramId
+
+        });
+
+
+
+        if(!user){
+
+
+            return res.json({
+
+                message:"User not found"
+
+            });
+
+
+        }
+
+
+
+        res.json(user);
+
+
+
+    }
+
+
+    catch(error){
+
+
+        res.status(500).json({
+
+            error:error.message
+
+        });
+
+
+    }
+
+
+});
+
+
+
+
+
+// ساخت کاربر جدید
+
+router.post("/", async(req,res)=>{
+
+
+    try{
+
+
+        const user = await User.create(
+
+            req.body
+
+        );
+
+
+        res.json({
+
+            success:true,
+
+            user:user
+
+        });
+
+
+
+    }
+
+
+    catch(error){
+
+
+        res.status(500).json({
+
+            error:error.message
+
+        });
+
+
+    }
+
+
+});
+
+
+
+
+
+export default router;
