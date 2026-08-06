41b38b01f8a699cb40766ae6d4c3b57a956b7ac8
Mahxchc <m.davari138600@gmail.com>
2026-08-06 20:01:36 +0330

    Update Wallet.js

--- routes/Wallet.js
+++ routes/Wallet.js
@@ -1,1 +1,78 @@
+// =====================================
+// AutoTrade AI
+// Wallet Routes
+// =====================================
 
+import express from "express";
+import Wallet from "../models/Wallet.js";
+
+
+const router = express.Router();
+
+
+
+// دریافت کیف پول کاربر
+
+router.get("/:userId", async (req, res)=>{
+
+
+    try{
+
+
+        const wallet = await Wallet.findOne({
+
+            userId: req.params.userId
+
+        });
+
+
+
+        if(!wallet){
+
+
+            return res.json({
+
+                balance:0,
+
+                currency:"USDT",
+
+                message:
+                "Wallet not found"
+
+
+            });
+
+
+        }
+
+
+
+
+        res.json(wallet);
+
+
+
+    }
+
+
+    catch(error){
+
+
+        res.status(500).json({
+
+            error:error.message
+
+        });
+
+
+    }
+
+
+});
+
+
+
+
+
+
+export default router;
