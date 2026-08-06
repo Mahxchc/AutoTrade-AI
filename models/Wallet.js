3a17ae42e7b83a6b0201638514233160d5a852ee
Mahxchc <m.davari138600@gmail.com>
2026-08-06 19:53:12 +0330

    Update Wallet.js

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,1 +1,69 @@
+// =====================================
+// AutoTrade AI
+// Wallet Model
+// =====================================
 
+import mongoose from "mongoose";
+
+
+const walletSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+
+    currency: {
+        type: String,
+        default: "USDT"
+    },
+
+
+    balance: {
+        type: Number,
+        default: 0
+    },
+
+
+    lockedBalance: {
+        type: Number,
+        default: 0
+    },
+
+
+    totalDeposit: {
+        type: Number,
+        default: 0
+    },
+
+
+    totalWithdraw: {
+        type: Number,
+        default: 0
+    },
+
+
+    lastUpdate: {
+        type: Date,
+        default: Date.now
+    },
+
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+
+});
+
+
+const Wallet = mongoose.model(
+    "Wallet",
+    walletSchema
+);
+
+
+export default Wallet;
