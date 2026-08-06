a15b454ce7f847f65b37da5e3f74010172b8cfd2
Mahxchc <m.davari138600@gmail.com>
2026-08-06 19:45:29 +0330

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,71 @@
+// =====================================
+// AutoTrade AI
+// User Model
+// =====================================
 
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+
+    telegramId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+
+    username: {
+        type: String,
+        default: ""
+    },
+
+
+    firstName: {
+        type: String,
+        default: ""
+    },
+
+
+    balance: {
+        type: Number,
+        default: 0
+    },
+
+
+    totalProfit: {
+        type: Number,
+        default: 0
+    },
+
+
+    todayProfit: {
+        type: Number,
+        default: 0
+    },
+
+
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+
+});
+
+
+
+const User = mongoose.model(
+    "User",
+    userSchema
+);
+
+
+
+export default User;
