48ec7206f66509883e32a965e4e75715b1ed2a02
Mahxchc <m.davari138600@gmail.com>
2026-08-06 19:49:50 +0330

    Update Trade.js

--- models/Trade.js
+++ models/Trade.js
@@ -1,1 +1,89 @@
+// =====================================
+// AutoTrade AI
+// Trade Model
+// =====================================
 
+import mongoose from "mongoose";
+
+
+const tradeSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+
+    symbol: {
+        type: String,
+        required: true
+    },
+
+
+    market: {
+        type: String,
+        default: "crypto"
+    },
+
+
+    type: {
+        type: String,
+        enum: [
+            "BUY",
+            "SELL"
+        ],
+        required: true
+    },
+
+
+    entryPrice: {
+        type: Number,
+        default: 0
+    },
+
+
+    exitPrice: {
+        type: Number,
+        default: 0
+    },
+
+
+    amount: {
+        type: Number,
+        default: 0
+    },
+
+
+    profit: {
+        type: Number,
+        default: 0
+    },
+
+
+    status: {
+        type: String,
+        enum: [
+            "OPEN",
+            "CLOSED"
+        ],
+        default: "OPEN"
+    },
+
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+
+});
+
+
+const Trade = mongoose.model(
+    "Trade",
+    tradeSchema
+);
+
+
+export default Trade;
