ac87adfe11ff2bdfa91a684e2488fd6bdf9ab545
Mahxchc <m.davari138600@gmail.com>
2026-08-07 04:11:59 +0330

    Update tradingService.js

--- services/tradingService.js
+++ services/tradingService.js
@@ -1,1 +1,108 @@
+// =====================================
+// AutoTrade AI
+// Trading Service
+// Execution Layer
+// =====================================
 
+
+const {
+    placeOrder
+} = require("./exchangeService");
+
+
+
+
+
+// اجرای معامله
+
+async function executeTrade({
+
+    signal,
+
+    symbol,
+
+    amount
+
+}) {
+
+
+
+    if(
+        !signal ||
+        !symbol ||
+        !amount
+    ){
+
+        throw new Error(
+            "Invalid trade data"
+        );
+
+    }
+
+
+
+
+    // اگر AI گفت صبر کن، معامله نکن
+
+    if(signal === "WAIT"){
+
+
+        return {
+
+
+            executed:false,
+
+
+            reason:
+            "AI decided to wait"
+
+
+        };
+
+
+    }
+
+
+
+
+
+
+    const order =
+    await placeOrder({
+
+        symbol,
+
+        type:signal,
+
+        amount
+
+    });
+
+
+
+
+
+
+    return {
+
+
+        executed:true,
+
+
+        order
+
+
+    };
+
+}
+
+
+
+
+module.exports = {
+
+
+    executeTrade
+
+
+};
