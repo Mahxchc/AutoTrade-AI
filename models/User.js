60dd97db004338481451f579096fa0723f17db8c
Mahxchc <m.davari138600@gmail.com>
2026-08-07 04:29:03 +0330

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,51 +9,69 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
 
     telegramId: {
+
         type: String,
+
         required: true,
+
         unique: true
+
     },
 
 
     username: {
+
         type: String,
+
         default: ""
+
     },
 
 
     firstName: {
+
         type: String,
+
         default: ""
+
     },
 
 
-    balance: {
-        type: Number,
-        default: 0
+    walletId: {
+
+        type: mongoose.Schema.Types.ObjectId,
+
+        ref: "Wallet",
+
+        default: null
+
     },
 
 
-    totalProfit: {
-        type: Number,
-        default: 0
-    },
+    botActive: {
 
-
-    todayProfit: {
-        type: Number,
-        default: 0
-    },
-
-
-    isActive: {
         type: Boolean,
+
         default: false
+
     },
 
 
     createdAt: {
+
         type: Date,
+
         default: Date.now
+
+    },
+
+
+    lastLogin: {
+
+        type: Date,
+
+        default: Date.now
+
     }
 
 
